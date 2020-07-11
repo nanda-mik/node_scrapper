6ad7e8b7cf567291bc72b2d84efd51313ea6d762
nanda-mik <nandamikushubha@gmail.com>
2020-07-11 16:22:01 +0000

    concurrently added

---
+++ client/build/precache-manifest.aeff88827de28b75e79efe3ab6a82e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15d996718ade8605ff8219a3c70bd16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0f8e96626528df76995",
+    "url": "/static/css/main.f849f4fd.chunk.css"
+  },
+  {
+    "revision": "e8f88da56ec60b79fb3d",
+    "url": "/static/js/2.dbf1e5b0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dbf1e5b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f8e96626528df76995",
+    "url": "/static/js/main.a3c89f22.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
